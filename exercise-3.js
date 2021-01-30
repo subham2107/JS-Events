5cffcae3eafa1cc2d8a097d425ee8ccbe1812195
Subham Mitra <subham.mitra21@gmail.com>
2021-01-30 23:03:26 +0530

    URL encoder/decoder

---
+++ exercise-3.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+
+    const input=document.querySelector('#some-input');
+    const output=document.querySelector('#some-output');
+
+    const buttonEncode = document.querySelector('#button-encode');
+    buttonEncode.addEventListener('click', event => {
+            output.value=encodeURIComponent(input.value);
+    });
+    const buttonDecode = document.querySelector('#button-decode');
+    buttonDecode.addEventListener('click', event => {
+            output.value=decodeURIComponent(input.value);
+    });
+
+})
