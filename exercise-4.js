6b3ff8a36b9420cfea13e30f64955e65dba7cdb6
Subham Mitra <subham.mitra21@gmail.com>
2021-01-30 23:49:19 +0530

    character counter

---
+++ exercise-4.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const output=document.querySelector('#some-output');
+    const inputField = document.querySelector('#some-input');
+        inputField.addEventListener('input', event => {
+            output.innerHTML=inputField.value.length;       
+    });
+
+})
